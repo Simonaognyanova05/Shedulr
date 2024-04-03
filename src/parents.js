b25a7c50eea738ae9150f2f9802efa8b6d1f6cb7
Simonaognyanova05 <simonaognanova05@gmail.com>
2024-04-03 16:27:28 +0300

    Add getting data from database

--- src/parents.js
+++ src/parents.js
@@ -1,3 +1,6 @@
-module.exports = (req, res) => {
-    res.render('parents', { title: 'Parents page', user: req.session.user })
+const getParentComment = require('../requests/getParentsComment');
+
+module.exports = async (req, res) => {
+    const comment = await getParentComment();
+    res.render('parents', { title: 'Parents page', user: req.session.user, comment: comment})
 } 
