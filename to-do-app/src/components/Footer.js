2469cab46aa2c107f4e7180ea6de7fbdab2e8d0f
Simonaognyanova05 <simonaognanova05@gmail.com>
2024-09-22 13:49:50 +0300

    Add anvab and footer

---
+++ to-do-app/src/components/Footer.js
@@ -0,0 +1,9 @@
+export default function Footer() {
+    return (
+        <footer>
+            <div class="footer-content">
+                <p>&copy; 2024 To-Do App. Всички права запазени.</p>
+            </div>
+        </footer>
+    );
+}
---
+++ to-do-app/src/components/Navbar.js
@@ -0,0 +1,12 @@
+export default function Navbar() {
+    return (
+        <nav>
+            <ul>
+                <li><a href="login.html">Вход</a></li>
+                <li><a href="register.html">Регистрация</a></li>
+                <li><a href="create-task.html">Създай Задача</a></li>
+                <li><a href="task-list.html">Моите Задачи</a></li>
+            </ul>
+        </nav>
+    );
+}
