7499b96875bcd8f12b2d920fbf4ca7e15bc5996c
Simonaognyanova05 <simonaognanova05@gmail.com>
2025-01-30 19:33:11 +0200

    Change logout

--- to-do-app/src/components/Logout.js
+++ to-do-app/src/components/Logout.js
@@ -1,13 +1,26 @@
 import { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import { useAuth } from "../contexts/AuthContext";
+import { signOut } from "firebase/auth";
+import { auth } from "../config/firebaseConfig"; 
 
 export default function Logout() {
     const navigate = useNavigate();
     const { onLogout } = useAuth();
 
     useEffect(() => {
-        onLogout();
-        navigate('/');
-    }, [])
+        async function handleLogout() {
+            try {
+                await signOut(auth); 
+                onLogout(); 
+                navigate('/'); 
+            } catch (error) {
+                console.error("Error logging out:", error);
+                alert("Неуспешно излизане!");
+            }
+        }
+        handleLogout();
+    }, [navigate, onLogout]);
+
+    return null; 
 }
